80eda05fa5f299cf553058cae20d8a68c5c6ec1e
evanedsaazola <evanedsaazola@gmail.com>
2019-05-30 23:08:25 +0700

    testing index and analyze code

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<html>
-<head>
-    <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-</head>
-<body>
-
-Upload picture that need to be analyzed :
-        <form action="index.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="" id="fileToUpload">
-        <input type="submit" name="submit" value="Upload">
-        </form>
-
-
-<?php
+<?php 
 
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
@@ -24,138 +10,97 @@ use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=evansubmissionduastorage;AccountKey=D23NljMevP00ja9D/m3VAJ6DRfdYK1wUIfiR35jGc7x/Ydhz+BSamoTZniLqnVR3Un3gQouLDWECx7Wy508Jxg==;EndpointSuffix=core.windows.net";
-
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-
-$createContainerOptions = new CreateContainerOptions();
-$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-$createContainerOptions->addMetaData("key1", "value1");
-$createContainerOptions->addMetaData("key2", "value2");
-
 $containerName = "evansubmitdua".generateRandomString();
 
-$blobClient->createContainer($containerName, $createContainerOptions);
-
-$fileToUpload = false;
-
+$blobClient = BlobRestProxy::createBlobService($connectionString);
 if (isset($_POST['submit'])) {
-
-    $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]); 
+    $fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
     $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-    echo fread($content, filesize($fileToUpload));
-    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+    $blobClient->createBlockBlob($containerName,$fileToUpload,$content);
     header("Location: index.php");
-    
 }
-
-echo $fileToUpload;
 
 $listBlobsOptions = new ListBlobsOptions();
 $listBlobsOptions->setPrefix("");
-$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+$result = $blobClient->listBlobs($containerName,$listBlobsOptions);
 
-echo "<br>";
-echo "<br>";
+?>
 
-echo "<table>";
-echo "<tr>";
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-echo "<th>File Name</th>";
-echo "<th>URL</th>";
+            <title>Upload and Analyze Image Application</title>
 
-foreach($result->getBlobs() as $blob) {
-    echo "<td>".$blob->getName()."</td>";
-    echo "<td>".$blob->getUrl()."</td></tr>";
-}
+            <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+            <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+            <link href="starter-template.css" rel="stylesheet">
+    </head>
 
-echo "</table>";
+    <body>
+        <main role="main" class="container">
+            
+            <div class="starter-template">
+                <br>
+                <h1>Image Analyzer</h1>
+                <p class="lead">Choose pictures you want to analyze and click <b>Upload</b> <br> to start picture analyzing process, click <b>Analyze!</b> in pictures list.</p>    
+                <span class="border-top my-3"></span>
+            </div>
 
-?>  
+            <div class="mt-4 mb-2">
+                <form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+                    <input type="submit" name="submit" value="Upload">
+                </form>
+            </div>
 
-<script type="text/javascript">
-    function processImage() {
-        // **********************************************
-        // *** Update or verify the following values. ***
-        // **********************************************
- 
-        // Replace <Subscription Key> with your valid subscription key.
-        var subscriptionKey = "e7cda73397944696b5ff13df3ff95781";
- 
-        // You must use the same Azure region in your REST API method as you used to
-        // get your subscription keys. For example, if you got your subscription keys
-        // from the West US region, replace "westcentralus" in the URL
-        // below with "westus".
-        //
-        // Free trial subscription keys are generated in the "westus" region.
-        // If you use a free trial subscription key, you shouldn't need to change
-        // this region.
-        var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
- 
-        // Request parameters.
-        var params = {
-            "visualFeatures": "Categories,Description,Color",
-            "details": "",
-            "language": "en",
-        };
- 
-        // Display the image.
-        var sourceImageUrl = "<?php echo $url ?>"; 
-        document.querySelector("#sourceImage").src = sourceImageUrl;
- 
-        // Make the REST API call.
-        $.ajax({
-            url: uriBase + "?" + $.param(params),
- 
-            // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader(
-                    "Ocp-Apim-Subscription-Key", subscriptionKey);
-            },
- 
-            type: "POST",
- 
-            // Request body.
-            data: '{"url": ' + '"' + sourceImageUrl + '"}',
-        })
- 
-        .done(function(data) {
-            // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        })
- 
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Display error message.
-            var errorString = (errorThrown === "") ? "Error. " :
-                errorThrown + " (" + jqXHR.status + "): ";
-            errorString += (jqXHR.responseText === "") ? "" :
-                jQuery.parseJSON(jqXHR.responseText).message;
-            alert(errorString);
-        });
-    };
-</script>
- 
-<h1>Analyze image:</h1>
-Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-<br><br>
-Image to analyze:
-<input type="text" name="inputImage" id="inputImage"
-    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-<button onclick="processImage()">Analyze image</button>
-<br><br>
-<div id="wrapper" style="width:1020px; display:table;">
-    <div id="jsonOutput" style="width:600px; display:table-cell;">
-        Response:
-        <br><br>
-        <textarea id="responseTextArea" class="UIInput"
-                  style="width:580px; height:400px;"></textarea>
-    </div>
-    <div id="imageDiv" style="width:420px; display:table-cell;">
-        Source image:
-        <br><br>
-        <img id="sourceImage" width="400" />
-    </div>
-</div>
-</body>
+            <br>
+            <br>
+
+            <h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
+                <table class='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th>File Name</th>
+                            <th>File URL</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        
+                        do {
+                            foreach($result->getBlobs() as $blob) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $blob->getName() ?></td>
+                                    <td><?php echo $blob->getUrl() ?></td>
+                                    <td>
+                                        <form action="analyze.php" method="post">
+                                            <input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+                                            <input type="submit" name="submit" value="Analyze" class="btn btn-primary">
+                                        </form>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+                        }
+                        while($result->getContinuationToken());
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+            <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+            <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+            
+    </body>
 </html>
